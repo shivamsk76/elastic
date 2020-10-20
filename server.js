0004a9745e811c38b58758cbf6f5beca1ef5290d
shivamsk76 <st7692833267@gmail.com>
2020-10-20 17:12:13 +0530

    freshcode

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const PORT = process.env.port || 8080;
-
+const path = require('path');
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 app.get('/', (req, res) => {
   res.send('flowers smell nice');
